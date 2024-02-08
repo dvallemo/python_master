836a3014471fb8eac301313e86d81a29181c249a
David Valle <davidvalle421@gmail.com>
2024-02-07 23:27:30 -0500

    updated folders

---
+++ python_basics1/main.py
@@ -0,0 +1,22 @@
+#Fundamental Data Types
+
+# int
+# float
+# bool
+# str
+# list
+# tuple
+# set
+# dict
+
+#Classes -> custom types
+# SuperCar
+
+#Specialized Data Types from Modules
+
+# None #absence of value or nothing, like null in c++
+
+#int and float
+
+print(2+4)
+print(2 ** 2) #2 to the power 2
