881bc088fdea6799b24978166e1a10535c70647b
dvallemo <davidvalle421@gmail.com>
2024-08-07 23:03:41 -0400

    adding decrement

---
+++ python_basics1/decrement/decrement.py
@@ -0,0 +1,10 @@
+grocery_list = ['banana', 'apple', 'turkey', 'toothbrush', 'pasta']
+
+#print the grocery list in reverse, dcrementing each index
+
+j = len(grocery_list) - 1 # 4
+
+while j >=0: #4 <= 0 or the last element on index 4
+    print(grocery_list[j])
+    j -= 1
+print('end')
